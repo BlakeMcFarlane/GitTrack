8d867fdd201025a13b14a4405058d12db97b510a
Blake McFarlane <blakescapital@gmail.com>
2023-12-14 22:28:47 -0500

    1. Built out skeleton with react.

---
+++ src/components/Badges.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styling/badges.css'
+
+const Badges = () => {
+  return (
+    <div className='badges-container'>
+        <div className='label'>
+            <h1>Badges</h1>
+        </div>
+        <div className='badges-list'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Badges
---
+++ src/components/QuickFacts.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../styling/quick-facts.css'
+
+const QuickFacts = () => {
+  return (
+    <div className='facts-container'>
+        <div className='fact-box'>
+            <h1>
+                #
+            </h1>
+            <p>commits</p>
+        </div>
+        <div className='fact-box'>
+            <h1>
+                #
+            </h1>
+            <p>lines</p>
+        </div>
+        <div className='fact-box'>
+            <h1>
+                #
+            </h1>
+            <p>languages</p>
+        </div>
+        <div className='fact-box'>
+            <h1>
+                #
+            </h1>
+            <p>rank</p>
+        </div>
+    </div>
+  )
+}
+
+export default QuickFacts
